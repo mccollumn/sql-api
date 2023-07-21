1a0db1c8721b75d2f90996cae8e0bfb1cd56c657
Nick McCollum <nmccollum81@gmail.com>
2023-07-21 08:23:43 -0700

    initial commit

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+  user: "foo", // sql user
+  password: "foo", //sql user password
+  server: "127.0.0.1", // if it does not work try- localhost
+  database: "Products",
+  options: {
+    trustedconnection: true,
+    enableArithAbort: true,
+    instancename: "SQLEXPRESS", // SQL Server instance name
+  },
+  port: 55892,
+};
+
+module.exports = config;
